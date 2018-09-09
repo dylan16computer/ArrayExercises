264b2aac41757db2e5781b8984cca80aeee20060
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-09-10 11:31:46 +1200

    Completed Task 1 and Task 2

---
+++ Arrays/Task 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[10];
+            Console.WriteLine("--- Array ---\n\nEnter 10 numbers of your choice: ");
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"\nNumber {i + 1}: ");
+                int number = int.Parse(Console.ReadLine());
+                array1[i] = number;
+            }
+            Console.WriteLine($"Array:\n");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Number {i + 1}: {array1[i]}\n");
+            }
+        }
+    }
+}
---
+++ Arrays/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[1000];
+            Random randint = new Random();
+            for(int i = 0; i < 1000; i++)
+            {
+                array1[i] = randint.Next(1000, 9999);
+            }
+
+            int check;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                int total = 0;
+                check = array1[i];
+                for(int j = 0; j < array1.Length; j++)
+                {
+                    if (check == array1[j])
+                        total += 1;
+                }
+                Console.WriteLine($"The number {i} has occured {total} times");
+            }
+        }
+    }
+}
