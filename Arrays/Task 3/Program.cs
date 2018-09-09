4a3e3a3696df62f26f21d039a6d1cfe80f4130f2
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-09-10 11:43:45 +1200

    Task 3 Program.cs incomplete

--- Arrays/Task 3/Program.cs
+++ Arrays/Task 3/Program.cs
@@ -10,7 +10,15 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            
+            int[] array1 = new int[100];
+            Random randint = new Random();
+            for(int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = randint.Next(1111, 9999);
+            }
+            Console.WriteLine("Enter the number you want to find (between 1111 and 9999):\n");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The number {Array.IndexOf(array1, num)} is at index...");
         }
     }
 }
